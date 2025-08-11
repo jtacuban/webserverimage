68f6e10bfe93d6ab351d772775d99b911d556e64
jtacuban <jaketacuban@gmail.com>
2025-08-11 15:03:28 +0800

    Update config.php

--- data/class/config/config.php
+++ data/class/config/config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=lms19", $username, $password);
